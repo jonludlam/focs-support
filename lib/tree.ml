b70184ee8ad8058dff5200dd703342d87b677c56
Jon Ludlam <jon@recoil.org>
2019-09-17 15:50:29 +0100

    More tree work
    Signed-off-by: Jon Ludlam <jon@recoil.org>

--- lib/tree.ml
+++ lib/tree.ml
@@ -1,12 +1,12 @@
-type t = Lf | Br of int * t * t
+type 'a t = Lf | Br of 'a * 'a t * 'a t
 
-let printer (x : t) =
+let printer (str_of_alpha : 'a -> string) (x : 'a t) =
   let filename,oc = Filename.open_temp_file "tree" ".dot" in
   Printf.fprintf oc "digraph D {\n";
   let rec dump_nodes n = function
     | Lf -> Printf.fprintf oc "  Lf%d [label=\"Lf\"]\n" n
     | Br (x,t1,t2) ->
-      Printf.fprintf oc "  Br%d [label=%d]\n" n x;
+      Printf.fprintf oc "  Br%d [label=%s]\n" n (str_of_alpha x);
         dump_nodes (2*n+1) t1; 
         dump_nodes (2*n+2) t2
   in
@@ -14,14 +14,14 @@ let printer (x : t) =
   let dump_edges t =
     let rec inner n1 n2 = function
     | Lf -> Printf.fprintf oc "  Br%d -> Lf%d\n" n1 n2
-    | Br (x,t1,t2) ->
+    | Br (_,t1,t2) ->
         Printf.fprintf oc "  Br%d -> Br%d\n" n1 n2;
         inner n2 (2*n2+1) t1;
         inner n2 (2*n2+2) t2
     in
     match t with
     | Lf -> ()
-    | Br (x,t1,t2) -> inner 0 1 t1; inner 0 2 t2
+    | Br (_,t1,t2) -> inner 0 1 t1; inner 0 2 t2
   in
   dump_edges x;
   Printf.fprintf oc "}\n";
